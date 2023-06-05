1beb4907e9c2e0c5872d759cf00ffddf3bc03a3d
XarrrdaS <94650404+XarrrdaS@users.noreply.github.com>
2023-06-05 23:30:12 +0200

    Add files via upload

---
+++ LJUSKRISTALLEN/send.php
@@ -0,0 +1,18 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $to = "kacper.ecdl1@gmail.com";
+    //$to = "maria_linder@hotmail.com"; // this is your Email address
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $subject = "LJUSKRISTALLEN - meddelande: ". date("Y/m/d");
+    $message = "Name: " . $name . "\n Email: " . $email . "\nPhone number: " . $phone . "Message: \n\n". $message;
+
+    $headers = "From: " . $from;
+    mail($to,$subject,$message,$headers);
+    header('Location: index.html');
+    echo '<script>alert("Message sent correctly!")</script>';
+    }
+?>
